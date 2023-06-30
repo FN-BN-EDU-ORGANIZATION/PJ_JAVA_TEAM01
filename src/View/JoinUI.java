56ad03ff2663bd8ca4c44078d234dcc8ee945069
anchobb <sori2142@naver.com>
2023-06-30 14:02:17 +0900

    V0.0 front

---
+++ src/View/JoinUI.java
@@ -0,0 +1,127 @@
+package View;
+
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+
+public class JoinUI extends JFrame implements ActionListener{
+
+	JTable table;
+	JTextField txt1; //아이디
+	JTextField txt2; //비밀번호
+	JTextField txt3; //비밀번호확인
+	JTextField txt4; //이름
+	JTextField txt5; //주소
+	JTextField txt6; //핸드폰번호
+	JButton btn1; //가입버튼
+	JButton btn2; //뒤로가기버튼(로그인화면출력)
+	Label L_txt1; 
+	Label L_txt2;
+	Label L_txt3;
+	Label L_txt4;
+	Label L_txt5;
+	Label L_txt6;
+	
+	LoginUI loginUI;
+	MainGUI mainGUI;
+	
+	
+	
+	JoinUI(){
+		super("JOIN US!");
+		setBounds(100, 100, 400, 380);
+		
+		// 패널
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		
+		//
+		txt1 = new JTextField();
+		txt2 = new JTextField();
+		txt3 = new JTextField();
+		txt4 = new JTextField();
+		txt5 = new JTextField();
+		txt6 = new JTextField();
+		btn1 = new JButton("돌아가기");
+		btn2 = new JButton("회원가입");
+		L_txt1 = new Label("ID");
+		L_txt2 = new Label("PW");
+		L_txt3 = new Label("PW Check");
+		L_txt4 = new Label("NAME");
+		L_txt5 = new Label("ADDRESS");
+		L_txt6 = new Label("PHONE");
+		
+		//
+		txt1.setBounds(120,10,230,30);
+		txt2.setBounds(120,50,230,30);
+		txt3.setBounds(120,90,230,30);
+		txt4.setBounds(120,130,230,30);
+		txt5.setBounds(120,170,230,30);
+		txt6.setBounds(120,210,230,30);
+		btn1.setBounds(30,270,100,40);//뒤로
+		btn2.setBounds(250,270,100,40);//가입
+		L_txt1.setBounds(50,10,60,30);
+		L_txt2.setBounds(45,50,60,30);
+		L_txt3.setBounds(30,90,60,30);
+		L_txt4.setBounds(40,130,60,30);
+		L_txt5.setBounds(30,170,60,30);
+		L_txt6.setBounds(35,210,60,30);
+		
+		//EVENT
+		btn1.addActionListener(this);
+		btn2.addActionListener(this);
+		
+		//컴포넌트 패널에 추가
+		panel.add(txt1);
+		panel.add(txt2);
+		panel.add(txt3);
+		panel.add(txt4);
+		panel.add(txt5);
+		panel.add(txt6);
+		panel.add(btn1);
+		panel.add(btn2);
+		panel.add(L_txt1);
+		panel.add(L_txt2);
+		panel.add(L_txt3);
+		panel.add(L_txt4);
+		panel.add(L_txt5);
+		panel.add(L_txt6);
+		
+		
+		add(panel);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setVisible(true);
+		setResizable(false);
+		
+		//LoginUI
+//		loginUI = new LoginUI();
+//		loginUI.setVisible(false);
+		
+	}
+	
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==btn1) {
+			System.out.println("BACK_BTN");
+			mainGUI.setVisible(true);
+			this.setVisible(false);
+		}else if(e.getSource()==btn2) {
+			System.out.println("JOIN_BTN");
+		}
+		
+	}
+
+//	public static void main(String[] args) {
+//		new JoinUI();
+//	}
+	
+}
---
+++ src/View/LoginUI.java
@@ -0,0 +1,159 @@
+package View;
+
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+public class LoginUI extends JFrame implements ActionListener{
+
+	JTextField id_txt;
+	JTextField pw_txt;
+	JButton login_btn;
+	JButton join_btn;
+	Label L_id;
+	Label L_pw;
+	
+	
+	//
+	MainGUI maingui;
+	JoinUI joinUI;
+//	MemberUI membergui;
+//	UserUI usergui;
+	
+	// DB연결정보 저장용 변수
+		String id = "root";
+		String pw = "1234";
+		String url = "jdbc:mysql://localhost:3306/musicdb";
+
+		// JDBC참조변수
+		Connection conn = null; // DB연결용 참조변수
+		PreparedStatement pstmt = null; // SQL쿼리 전송용 참조변수
+		ResultSet rs = null; // SQL쿼리 결과(SELECT결과) 수신용 참조변수
+	
+	public LoginUI(){
+		super("LOGIN");
+		setBounds(400, 100, 300, 170);
+
+		// 패널
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+
+		
+		//
+		id_txt = new JTextField();
+		pw_txt = new JTextField();
+		login_btn = new JButton("로그인");
+		join_btn = new JButton("회원가입");
+		L_id = new Label(" ID");
+		L_pw = new Label("PW");
+		
+		//
+		id_txt.setBounds(40,15,140,30);
+		pw_txt.setBounds(40,50,140,30);
+		login_btn.setBounds(185,15,80,65);
+		join_btn.setBounds(40,90,225,30);
+		L_id.setBounds(10, 15, 30, 30);
+		L_pw.setBounds(10, 50, 30, 30);
+		
+		//EVENT
+		login_btn.addActionListener(this);
+		join_btn.addActionListener(this);
+		
+		
+		// 컴포넌트를 패널에 추가
+		panel.add(id_txt);
+		panel.add(pw_txt);
+		panel.add(login_btn);
+		panel.add(join_btn);
+		panel.add(L_id);
+		panel.add(L_pw);
+		 
+		
+		add(panel);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setVisible(true);
+		setResizable(false);
+		
+		//joinUI
+		joinUI = new JoinUI();
+		joinUI.setVisible(false);
+//		joinUI.setMainGUI(this);
+		
+		
+//		membergui = new MemberUI();
+//		usergui = new UserUI();
+		
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); // 드라이브 적재
+			System.out.println("Driver Loading Success..");
+			conn = DriverManager.getConnection(url, id, pw);
+			System.out.println("DB Connected..");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+            	
+            	
+            	maingui.setVisible(true);
+            }
+        });
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==login_btn) {
+			System.out.println("LOGIN_BTN");
+			//로그인 체크
+			
+			
+			//회원사서인지 판단해서 창띄우기
+			if(id_txt.getText().equals("1"))
+			{
+				this.setVisible(false);
+//				membergui.setVisible(true);
+			}
+			else if(id_txt.getText().equals("2"))
+			{
+				this.setVisible(false);
+//				usergui.setVisible(true);
+			}
+			 
+			
+		}
+		
+		if(e.getSource()==join_btn) {
+			System.out.println("JOIN_BTN");
+			joinUI.setVisible(true);
+			this.setVisible(false);
+		}
+		
+	}
+	
+
+	public void setMainGUI(MainGUI maingui) {
+		this.maingui = maingui;
+//		membergui.setMainUI(maingui);
+//		usergui.setMainUI(maingui);
+		
+		
+	}
+	
+}
